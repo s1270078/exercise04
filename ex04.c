f1ba3e07f427fb872940f3db7becbb5d0a8f953e
s1270078 <69299881+s1270078@users.noreply.github.com>
2020-08-06 19:31:57 +0900

    HoT: Initial revision

--- ex04.c
+++ ex04.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 
 int main(){
-int i,a;
-int heads=0,tails=0;
-char str[8];
+int i,x;
+int head=0,tail=0;
+char s[8];
 
 printf("Tossing a coin...\n");
 
 for(i=0;i<3;i++){
 a=rand();
-if(a%2==0){
+if(x%2==0){
 printf("Round %d: Head\n",i+1);
 head++;
 }
 else{
 printf("Round %d: Tail\n",i+1);
-tails++;
+tail++;
 }
 }
 
-printf("Heads: %d, Tails: %d\n", heads, tails);
+printf("Heads: %d, Tails: %d\n", head, tail);
 
 return 0;
 }
